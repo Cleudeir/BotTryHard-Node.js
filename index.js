afc2424e1e87a91e29870ca9ce7b43b3f5e92b42
Cleudeir <82124121+Cleudeir@users.noreply.github.com>
2022-03-14 13:18:50 -0300

    new form api request

--- index.js
+++ index.js
@@ -18,8 +18,9 @@ const bot = new Discord.Client({
 
 const Bot = async () => {
   bot.login(config.token);
-  bot.on('ready', () => {
+  bot.on('ready', (messageCreate) => {
     console.log('✔️  Bot foi iniciado');
+    return messageCreate;
   });
 
   const qnt = 5;
@@ -29,10 +30,10 @@ const Bot = async () => {
       .then((resp) => resp.json())
       .then((resp) => resp)
       .catch(() => []);
-    dataRanking = await data;
-
+      if(data){
+        dataRanking = await data;
+      }
     const players = [];
-
     for (let i = 0; i < dataRanking.length; i += 1) {
       if (dataRanking[i].matches < 50) { players.push(dataRanking[i].account_id); }
     }
@@ -52,22 +53,21 @@ const Bot = async () => {
     return data;
   }
   await pull();
+  setInterval(pull, qnt * 60 * 1000)
 
-  bot.on('message', async (message) => {
-    if (message.author.bot) return;
-    if (message.channel.type === 'dm') return;
-    if (!message.content.startsWith(config.prefix)) return;
+  bot.on('messageCreate', async (messageCreate) => {
+    if (messageCreate.author.bot) return;
+    if (messageCreate.channel.type === 'dm') return;
+    if (!messageCreate.content.startsWith(config.prefix)) return;
 
-    const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+    const args = messageCreate.content.slice(config.prefix.length).trim().split(/ +/g);
     const [comando, info] = args.shift().toLowerCase().split('=');
 
     // comando ping
-    if (comando === 'ping' || comando === 'p') {
-      const m = await message.channel.send('Ping?');
-      m.edit(`Ping! A Latência é ${m.createdTimestamp - message.createdTimestamp}ms.`);
+    if (comando === 'hello' || comando === 'h') {
+      await messageCreate.channel.send('Hello world!');
     } else if (comando === 'help' || comando === '?') {
-      const m = await message.channel.send('help?');
-      m.edit(`\n
+      await messageCreate.channel.send(`\n
       Commands:
       !p => Verifica seu ping
       !r=account_id => Verifica seu ranked e seu status médio
@@ -79,11 +79,10 @@ const Bot = async () => {
 
         if (playerData) {
           const img = `${playerData.avatarfull.slice(0, playerData.avatarfull.length - 9)}_medium.jpg`;
-          await message.channel.send({
+          await messageCreate.channel.send({
             files: [img],
           });
-          const m = await message.channel.send('Ranking...');
-          m.edit(
+          await messageCreate.channel.send(
             `Aqui esta  ${playerData.personaname}:
           ➡️ Position: ${playerData.id} de ${dataRanking.length}
           Rating : ${playerData.ranking.toLocaleString('pt-BR')}  
@@ -101,12 +100,12 @@ const Bot = async () => {
           `,
           );
         } else {
-          await message.channel.send(`
+          await messageCreate.channel.send(`
           Infelizmente você não esta no ranking
           busque no site: https://dota-try-hard.vercel.app/${info}`);
         }
       } else {
-        await message.channel.send('Desculpe! DataBase esta offline');
+        await messageCreate.channel.send('Desculpe! DataBase esta offline');
       }
     }
   });
