3726bd06c0fd8e6d33d4eb84109ff12b86a07d05
Cleudeir <cleudeir@gmail.com>
2022-03-19 16:49:35 -0300

    time request

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ const Bot = async () => {
 
     for (let n = 0; n < players.length; n += 1) {
       console.log(n, players.length);
-      sleep(60 * 1000);
+      await sleep(60 * 1000);
       const send = players[n];
       console.log('Busca', new Date().toLocaleTimeString('pt-BR', { timeZone: 'America/Sao_Paulo' }), send);
       const result = await fetch(`${config.url}/api/auto/${send}`);
